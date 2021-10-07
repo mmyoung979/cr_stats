04b3b1f51dc6b8f9ffd805eb6bafe7719e62694a
Vandivier <john@afterecon.com>
2021-10-07 18:56:01 -0400

    feat: refer to same domain api route

--- frontend/src/components/FetchApi.js
+++ frontend/src/components/FetchApi.js
@@ -9,7 +9,7 @@ const FetchApi = () => {
 
   React.useEffect(() => {
     const loadData = async () => {
-      const url = "http://localhost:5000/";
+      const url = `${window.location.href}/api`;
       const response = await fetch(url);
       const data = await response.json();
 
